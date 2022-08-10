302ed187466015d40836604e7c26b70af6f7f066
idd_dcarmo <idd_dcarmo@uolinc.com>
2022-08-10 19:12:33 -0300

    Add criteria query to restaurant

--- src/main/java/com/algaworks/api/algafood/repositories/RestaurantRepositoryImpl.java
+++ src/main/java/com/algaworks/api/algafood/repositories/RestaurantRepositoryImpl.java
@@ -3,10 +3,14 @@ package com.algaworks.api.algafood.repositories;
 import com.algaworks.api.algafood.domain.model.Restaurant;
 import com.algaworks.api.algafood.domain.repositories.CustonsRestaurantRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @Repository
 public class RestaurantRepositoryImpl implements CustonsRestaurantRepository {
@@ -16,13 +20,26 @@ public class RestaurantRepositoryImpl implements CustonsRestaurantRepository {
 
     @Override
     public List<Restaurant> findByNameAndTaxs(String name, Double initialTax, Double finalTax){
-        final var jpql = "from Restaurant where name like :name" +
-                "and deliveryTax between :initialTax and :finalTax";
 
-        return entityManager.createQuery(jpql, Restaurant.class)
-                .setParameter("name", "%" + name + "%")
-                .setParameter("initialTax", initialTax)
-                .setParameter("finalTax", finalTax)
-                .getResultList();
+        final var criteriaBuilder = entityManager.getCriteriaBuilder();
+
+        final var criteriaQuery = criteriaBuilder.createQuery(Restaurant.class);
+
+        final var rootRestaurant = criteriaQuery.from(Restaurant.class);
+        final var arrayList = new ArrayList<Predicate>();
+
+        if(StringUtils.hasText(name)){
+            arrayList.add(criteriaBuilder.like(rootRestaurant.get("name"), "%" + name + "%"));
+        }
+
+        if(Objects.isNull(initialTax) && Objects.isNull(finalTax)){
+            arrayList.add(criteriaBuilder.between(rootRestaurant.get("tax"), initialTax, finalTax));
+        }
+
+        criteriaQuery.where(arrayList.toArray(new Predicate[0]));
+
+        final var  query = entityManager.createQuery(criteriaQuery);
+
+        return query.getResultList();
     }
 }
